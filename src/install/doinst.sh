8fe4e948e11a2801f1ec2408bf784128b788c8d0
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-08-10 03:22:03 +0000

    feat: add older version of storcli for extra hardware support

--- src/install/doinst.sh
+++ src/install/doinst.sh
@@ -1,1 +1,3 @@
-( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64 storcli )
+( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3404 storcli )
+( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3404 storcli64 )
+( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3205 storcli-3205 )
