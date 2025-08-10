f1696dc51f2f1906edc5475a20a101eaab3610b5
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2025-08-10 03:28:54 +0000

    feat: add storcli2

--- src/install/doinst.sh
+++ src/install/doinst.sh
@@ -1,3 +1,4 @@
 ( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3404 storcli )
 ( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3404 storcli64 )
 ( cd usr/local/bin ; ln -sf /usr/local/bin/storcli64-3205 storcli-3205 )
+( cd usr/local/bin ; ln -sf /usr/local/bin/storcli2Lite-8.14 storcli2 )
